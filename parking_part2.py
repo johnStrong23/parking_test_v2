7be44d3dc547be8861cd568e186220e2e3cb096f
johnStrong23 <88659692+johnStrong23@users.noreply.github.com>
2021-08-16 15:52:58 +0300

    Update parking_part2.py

--- parking_part2.py
+++ parking_part2.py
@@ -4,16 +4,9 @@
 
 #comment added for MILTOS to see if need be
 
-!pip install pyyaml==5.1
-
-# workaround: install old version of pytorch since detectron2 hasn't released packages for pyTorch 1.9
-!pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-
 # install detectron2 that matches pytorch 1.8 
 !pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
 # **Run a Pre-Trained Detectron2 Model**
-
-#Perform **Authorization**
 
 from google.colab import drive
 drive.mount('/content/drive/')
